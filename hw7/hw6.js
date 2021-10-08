ee251a447f60ad064d59da859b74fadcb199757c
NikaK <nika.koledova@yandex.ru>
2021-10-09 02:06:03 +0300

    process hw7

---
+++ hw7/hw6.js
@@ -0,0 +1,29 @@
+//1 level
+//1
+function task_lvl1_1() { document.location.href = "./lvl1/1-перебор_свойств_объекта.html"; }
+
+function task_lvl1_2() { document.location.href = "./lvl1/2-очистка_с_помощью_innerHTML.html"; }
+
+function task_lvl1_3() { document.location.href = "./lvl1/3-заполнение_с_конкатенацией.html"; }
+
+function task_lvl1_4() { document.location.href = "./lvl1/4-вставка_разметки.html"; }
+
+//2
+
+function task_lvl2_1() { document.location.href = "./lvl2/1-получение_data-атрибутов.html"; }
+
+function task_lvl2_2() { document.location.href = "./lvl2/2-создание_разметки.html"; }
+
+function task_lvl2_3() { document.location.href = "./lvl2/3-размер_шрифта.html"; }
+
+//3
+
+function task_lvl3_1() { document.location.href = "./lvl3/1-tabs/1-tabs.html"; }
+
+function task_lvl3_2() { document.location.href = "./lvl3/2-сообщение/message.html"; }
+
+//4
+
+function task_lvl4_1() { document.location.href = "./lvl4/1 вывод товаров/1-вывод_товаров.html"; }
+
+function task_lvl4_2() { document.location.href = "./lvl4/2 прогрессбар/progress.html"; }
