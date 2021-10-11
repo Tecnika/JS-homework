288902d3f082feb04dd82c54c2b3f5cf8f8cb1e5
NikaK <nika.koledova@yandex.ru>
2021-10-12 00:54:38 +0300

    hw7

--- hw7/lvl4/1 вывод товаров/app.js
+++ hw7/lvl4/1 вывод товаров/app.js
@@ -11,8 +11,7 @@
 */
 
 const products = {
-    phones: [
-        {
+    phones: [{
             id: 1,
             name: "Смартфон 1",
             price: "23,99 р.",
@@ -32,8 +31,7 @@ const products = {
         },
     ],
 
-    tablets: [
-        {
+    tablets: [{
             id: 4,
             name: "Планшет 4",
             price: "99,99 р.",
@@ -47,8 +45,7 @@ const products = {
         },
     ],
 
-    tv: [
-        {
+    tv: [{
             id: 6,
             name: "Телевизор 6",
             price: "199,99 р.",
@@ -74,7 +71,7 @@ const products = {
         },
     ],
 };
-
+let products = document.querySelector('.products');
 
 /**
  * Эта функция должна вызываться при клике по кнопкам.
@@ -82,10 +79,10 @@ const products = {
  */
 function clickHandler(event) {
     //вам нужно очищать содержимое .products
-    
+    products.innerHTML = " ";
     //в showCategory надо передать строку с типом категории, тип берите
     //из атрибута data-type у кнопки, по которой кликнули.
-    
+
 }
 
 /**
@@ -96,7 +93,7 @@ function clickHandler(event) {
  * по которой кликнули.
  */
 function showCategory(category) {
-    
+
 }
 
 /**
